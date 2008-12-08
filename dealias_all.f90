d552329f790eb088ed7bf19824647cb9897726ee
schumakov <schumakov@b775aca2-af6d-11dd-b4f7-7faeb9b304ab>
2008-12-08 23:51:34 +0000

    Changed the dealias_all to reflect teh change in dealiasing outlined in the previous update.

--- dealias_all.f90
+++ dealias_all.f90
@@ -6,6 +6,7 @@ subroutine dealias_all
   implicit none
 
   integer :: i,j,k
+  real*8  :: akmax
 
   ! "2/3-rule".  For good explanation, see the following paper:
   ! R.S.Rogallo, "Numerical Exoeriments in Homogeneous Turbuelnce"
@@ -15,10 +16,14 @@ subroutine dealias_all
   ! truncating all the modes in which at least one component of the k-vector
   ! has magnitude that is larger than N/3
 
+  akmax = real(kmax,8)
+
   do k = 1,nz
      do j = 1,ny
         do i = 1,nx+2
-           if ( akx(i)**2 + aky(k)**2 + akz(j)**2 .gt. real(kmax,8)**2) then
+           if  (abs(akx(i)) .gt. akmax .or. &
+                abs(aky(k)) .gt. akmax .or. &
+                abs(akz(j)) .gt. akmax) then
               fields(i,j,k,1:3+n_scalars) = zip
            end if
 
