592f0278c0fcce1e3e7559ba626f28715e81cc06
schumakov <schumakov@b775aca2-af6d-11dd-b4f7-7faeb9b304ab>
2009-02-06 22:58:43 +0000

    Changed the real*4 output.  Now the variables are first truncated in Fourier space so they don't have any frequencies higher than kmax, then converted to real space and written out.  This way we ensure that the real*4 data is truly isotropic.

--- io_write_4.f90
+++ io_write_4.f90
@@ -10,43 +10,57 @@ subroutine io_write_4
 
   implicit none
 
-  integer :: n
+  integer :: n_out, n, i, j, k
+  real*8  :: wmag2, rkmax2
+
+  ! every variable will undergo a mode truncation for all modes
+  ! that are higher than kmax.  This will ensure that the written
+  ! variables are isotropic
+
+  rkmax2 = real(kmax,8)**2
+
+  ! number of variables to write out
+  n_out = 3
+  if (int_scalars) n_out = n_out + n_scalars
+
+  ! putting all variables in wrk array
+  do k = 1,nz
+     do j = 1,ny
+        do i = 1,nx+2
+           wmag2 = akx(i)**2 + aky(k)**2 + akz(j)**2
+
+           if (wmag2 .gt. rkmax2) then
+              wrk(i,j,k,1:n_out) = fields(i,j,k,1:n_out)
+           else
+              wrk(i,j,k,1:n_out) = zip
+           end if
+
+        end do
+     end do
+  end do
 
   ! velocities
-  wrk(:,:,:,1) = fields(:,:,:,1)
   call xFFT3d(-1,1)
   fname = 'u.'//file_ext
   tmp4(1:nx,1:ny,1:nz) = wrk(1:nx,1:ny,1:nz,1)
-!!$  write(out,*) "Writing u with all procs"
-!!$  call flush(out)
   call write_tmp4_all
 
-!!$  fname = 'u1.'//file_ext
-!!$  write(out,*) "Writing u with one proc"
-!!$  call flush(out)
-!!$  call write_tmp4
-!!$  write(out,*) "wrote."
-!!$  call flush(out)
-
-  wrk(:,:,:,1) = fields(:,:,:,2)
-  call xFFT3d(-1,1)
+  call xFFT3d(-1,2)
   fname = 'v.'//file_ext
-  tmp4(1:nx,1:ny,1:nz) = wrk(1:nx,1:ny,1:nz,1)
+  tmp4(1:nx,1:ny,1:nz) = wrk(1:nx,1:ny,1:nz,2)
   call write_tmp4_all
 
-  wrk(:,:,:,1) = fields(:,:,:,3)
-  call xFFT3d(-1,1)
+  call xFFT3d(-1,3)
   fname = 'w.'//file_ext
-  tmp4(1:nx,1:ny,1:nz) = wrk(1:nx,1:ny,1:nz,1)
+  tmp4(1:nx,1:ny,1:nz) = wrk(1:nx,1:ny,1:nz,3)
   call write_tmp4_all
 
   ! scalars
   if (int_scalars) then
      do n = 1,n_scalars
-        wrk(:,:,:,1) = fields(:,:,:,3+n)
-        call xFFT3d(-1,1)
+        call xFFT3d(-1,3+n)
         write(fname,"('sc',i2.2,'.',a6)") n,file_ext
-        tmp4(1:nx,1:ny,1:nz) = wrk(1:nx,1:ny,1:nz,1)
+        tmp4(1:nx,1:ny,1:nz) = wrk(1:nx,1:ny,1:nz,3+n)
         call write_tmp4_all
 
      end do
